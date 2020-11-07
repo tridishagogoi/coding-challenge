054f459bd92223408ccd3e578078f2fd4c2c8c2a
Tridisha Gogoi <67456385+tridishagogoi@users.noreply.github.com>
2020-11-08 01:07:20 +0530

    Add files via upload

---
+++ Py5.py
@@ -0,0 +1,11 @@
+#Create a program that displays your name and complete mailing address formatted in the manner that you would usually see it on the outside of an envelope.
+#Your program needs to read any input from the user
+
+a=str(input())
+b=str(input())
+c=str(input())
+d=int(input())
+print (a)
+print (b)
+print (c)
+print (d)
