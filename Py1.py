66094aa654d34e9df3d7971492fe00bf233116f7
Tridisha Gogoi <67456385+tridishagogoi@users.noreply.github.com>
2020-11-08 00:53:59 +0530

    Add files via upload

---
+++ Py1.py
@@ -0,0 +1,14 @@
+#Write a program to read the address of a user. Display the result by breaking it into multiple lines
+#Input and Output Format:
+#Refer sample input and output for formatting specification.
+#All float values are displayed correct to 2 decimal places.
+#All text in bold corresponds to input and the rest corresponds to output
+
+a=int(input(""))
+b=input("")
+c=int(input(""))
+print("Address")
+print("House Number:",a)
+print("Area:",b)
+print("Pin Code:",c)
+​
